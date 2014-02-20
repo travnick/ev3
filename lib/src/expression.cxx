c4eef1414aee806ac21675a9e00189097b29a7a5
xantares <xantares@fujitsu-l64>
2014-02-20 23:29:19 +0100

    +test

--- lib/src/expression.cxx
+++ lib/src/expression.cxx
@@ -5878,7 +5878,6 @@ bool Simplify(Expression* a)
     (*a)->ConsolidateProductCoeffs();
     (*a)->DistributeCoeffOverSum();
     ret = DifferenceToSum(a);
-    std::cout << "Simplify"<<std::endl;
     if (ret)
     {
       changedflag = true;
